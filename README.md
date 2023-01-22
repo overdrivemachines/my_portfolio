5239fd614cd85b5871655f09dbf3f50e82f32cf6
Dipen Chauhan <get.dipen@gmail.com>
2023-01-22 00:10:18 -0800

    adding about me

--- README.md
+++ README.md
@@ -9,4 +9,26 @@ Building my portfolio site dipenchauhan.com
 - Boxicons 2.1.4 - for icons (similar to Font Awesome)
 - Animate.css 4.1.1
 
+# TODO
+
+- Content
+  - I am available for hire and open to any ideas of cooperation.
+  - I want to make things that make a difference.
+  - As a web developer, I enjoy using my obsessive attention to detail, my unequivocal love for making things, and my mission-driven work ethic to literally change the world. That's why I’m excited to make a big impact at a high growth company.
+  - Here are a few technologies I’ve been working with recently:
+    - JavaScript (ES6+), Node.js, Eleventy
+  - Im always interested about:
+    UI/UX Design, Backend Development, New Businesses, Startups, ... https://bepatrickdavid.com/
+  - Contact - Have a question or want to work together?
+- Jobs - https://arc.dev/
+
 # References
+
+- Awesome Portfolio Examples
+  - https://olaolu.dev/
+  - https://robbowen.digital/
+  - https://www.cassie.codes/
+  - https://www.seyi.dev/
+  - https://tamalsen.dev/
+  - https://armaizadenwala.com/
+  - https://jhey.dev/
--- assets/js/my_script.js
+++ assets/js/my_script.js
@@ -45,37 +45,24 @@ navSlide();
    mobile-drop-down
 --------------------------------------------- */
 
-$(".main-nav-js .bi").on("click", function (event) {
-  var $fl = $(this);
-  $(this).parent().siblings().find(".sub-menu").slideUp();
-
-  $(this).parent().siblings().find(".bi").addClass("bi-plus");
-  if ($fl.hasClass("bi-plus")) {
-    $fl.removeClass("bi-plus").addClass("bi-dash");
-  } else {
-    $fl.removeClass("bi-dash").addClass("bi-plus");
-  }
-
-  $fl.next(".sub-menu").slideToggle();
-});
-
-// FIXED SIDEBAR
-
-// $(".fixed-sidebar .bi").on('click', function (event) {
+// $(".main-nav-js .bi").on("click", function (event) {
 //   var $fl = $(this);
-//   $(this).parent().siblings().find('.sub-menu').slideUp();
+//   $(this).parent().siblings().find(".sub-menu").slideUp();
 
-//   $(this).parent().siblings().find('.bi').addClass('bi-plus');
-
-//   if ($fl.hasClass('bi-plus')) {
-//       $fl.removeClass('bi-plus').addClass('bi-dash');
+//   $(this).parent().siblings().find(".bi").addClass("bi-plus");
+//   if ($fl.hasClass("bi-plus")) {
+//     $fl.removeClass("bi-plus").addClass("bi-dash");
 //   } else {
-//       $fl.removeClass('bi-dash').addClass('bi-plus');
-//   }
-//   var siblings = $(this).parent().siblings().find('.bi');
-//   if( siblings.hasClass('bi-dash') ) {
-//     siblings.removeClass('bi-dash');
+//     $fl.removeClass("bi-dash").addClass("bi-plus");
 //   }
 
 //   $fl.next(".sub-menu").slideToggle();
 // });
+
+/* ---------------------------------------------
+     circletype
+--------------------------------------------- */
+let CircleTypeText1 = document.getElementById("CircleTypeText1");
+if (CircleTypeText1 != null) {
+  new CircleType(CircleTypeText1);
+}
